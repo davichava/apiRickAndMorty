80be90a9611780ebec2d474560b570838c5aedfc
david <57021720+davichava@users.noreply.github.com>
2021-08-23 16:04:16 -0500

    Update conexion.js

--- config/conexion.js
+++ config/conexion.js
@@ -1,5 +1,7 @@
 const { createPool } = require('mysql')
 
+// se crea la coneccion a la base de datos Mysql
+// se digitan las credenciales para la conexion
 const connetc = createPool({
     host: 'localhost',
     user: 'root',
